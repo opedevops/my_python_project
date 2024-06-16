2e1a089525bb7cce9010584eebb1323b5b75bee5
olatide alli-balogun <olatidealli-balogun@olatides-MacBook-Pro-957.local>
2024-06-16 08:04:38 -0500

    enhancement: added user_name to main.py file

--- main.py
+++ main.py
@@ -4,13 +4,13 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
+def print_hi(name, user_name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}') # Press ⌘F8 to toggle the breakpoint.
+    print(f'Hi, {name} from {user_name}!!') # Press ⌘F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('PyCharm', 'Ope')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
